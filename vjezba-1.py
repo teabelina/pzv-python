d760d3a6677aed9d0b09089a00f10cf2fffd9b51
teabelina <114573060+teabelina@users.noreply.github.com>
2022-09-28 11:24:45 +0200

    Add files via upload

---
+++ vjezba-1.py
@@ -0,0 +1,10 @@
+print("hello world!")
+print("ucimo python")
+ime = input("kako se zoves?")
+print("pozdrav " + ime)
+x = 8
+print(x)
+pi = 3.14
+print(pi)
+poruka = "ovo su znakovi- Stringovi"
+print(poruka)
---
+++ vjezba-3.py
@@ -0,0 +1,12 @@
+ime= input("unesi svoje ime:")
+print ("pozdrav, " + ime)
+
+a= int(input("unesi broj a: "))
+b= int(input ("unesi broj b: "))
+zbroj=a+b
+print("zbroj brojeva {0} i {1} iznosi {2}". format(a,b, zbroj))
+razlika=a-b
+print("razlika brojeva {0} i {1} iznosi {2}". format(a,b, razlika))
+kolicnik=a/b
+print("kolicnik brojeva {0} i {1} iznosi {2}". format(a,b, kolicnik))
+
